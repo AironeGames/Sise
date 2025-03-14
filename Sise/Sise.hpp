6c2129396242efc5fbfc1c7bfd0640217b24161a
AironeGames <renat.aslanyan@gmail.com>
2025-03-14 20:00:28 +0300

    1.0.0 (first)
    First Commit

---
+++ Sise/Sise.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <stdarg.h>
+
+#include "Sise/Sise/Defines.hpp"
+#include "Sise/Sise/Std.hpp"
+#include "Sise/Sise/Classes.hpp"
+#include "Sise/Sise/Unfinished.hpp"
---
+++ Sise/Unfinished.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+//template <typename A>
+//class list {
+//public:
+//	list(int c, A a, ...) {
+//		this->col = c;
+//		A* arr = new A[c];
+//		va_list ptr;
+//
+//		va_start(ptr, a);
+//		for (int i = 0; i < c; i++) {
+//			arr[i] = a;
+//			a = va_arg(ptr, A);
+//
+//		}
+//		va_end(ptr);
+//
+//		for (int i = 0; i < c; i++) {
+//			this->pArr[i] = arr[i];
+//
+//		}
+//
+//	}
+//
+//	~list() {
+//		delete[] pArr;
+//
+//	}
+//public:
+//	int col;
+//	A* pArr = new A[col];
+//
+//};
+
+//std::istream& operator>>(std::istream& is, futy& other) {
+//	is >> other.val;
+//	return is;
+//
+//}
+
+//class test1
+//{
+//public:
+//	template <typename A>
+//	test1(int colVo, A first, ...) {
+//		int *ptr = &first;
+//		while (colVo--) {
+//
+//
+//		}
+//
+//	}
+//	~test1() {
+//
+//
+//	}
+//
+//private:
+//	int* mass = new int[6];
+//
+//};
+
